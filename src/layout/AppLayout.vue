046cdab963fe129906e15c780fff001dd5f9622b
Adarsh T <adarsh.t@macys.com>
2025-02-21 22:36:56 +0530

    DaisyUI Header & Footer Integration

--- src/layout/AppLayout.vue
+++ src/layout/AppLayout.vue
@@ -1,9 +1,13 @@
 <template>
+  <AppHeader />
   <RouterView />
+  <AppFooter />
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router'
+import AppHeader from '@/layout/AppHeader.vue'
+import AppFooter from '@/layout/AppFooter.vue'
 </script>
 
 <style scoped></style>
