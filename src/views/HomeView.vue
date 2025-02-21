1930dbb00bd8875e39a4de844eeedc794eec401c
Adarsh T <adarsh.t@macys.com>
2025-02-21 17:04:49 +0530

    Added Daisy UI, Removed unwanted codes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <h1>This is an home page</h1>
+  </div>
 </template>
+
+<style></style>
