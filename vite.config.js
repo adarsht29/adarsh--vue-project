7faa760fbebdba663d09d0574030838c33fd9314
Adarsh T <adarsh.t@macys.com>
2025-02-27 23:57:34 +0530

    Added App version to Footer

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import packageJson from './package.json'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,5 +14,8 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  define: {
+    'import.meta.env.VITE_APP_VERSION': JSON.stringify(packageJson.version),
   },
 })
