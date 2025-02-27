5ce1be0d20414af75805c43daa46dd2bf54e2419
Adarsh T <adarsh.t@macys.com>
2025-02-27 23:41:13 +0530

    Header & Footer icon, link style changes

--- src/layout/AppHeader.vue
+++ src/layout/AppHeader.vue
@@ -1,68 +1,81 @@
 <template>
   <div class="navbar bg-base-100">
     <div class="flex-1">
-      <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
+      <input id="navbar-drawer" type="checkbox" class="drawer-toggle" />
       <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
-        <label for="my-drawer-3" aria-label="open sidebar">
+        <label for="navbar-drawer" aria-label="open sidebar">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
             fill="none"
             viewBox="0 0 24 24"
-            stroke="currentColor"
+            class="inline-block h-6 w-6 stroke-current"
           >
             <path
               stroke-linecap="round"
               stroke-linejoin="round"
               stroke-width="2"
-              d="M4 6h16M4 12h8m-8 6h16"
-            />
+              d="M4 6h16M4 12h16M4 18h16"
+            ></path>
           </svg>
         </label>
       </div>
 
       <div class="drawer-side">
-        <label for="my-drawer-3" aria-label="close sidebar" class="drawer-overlay"></label>
-        <ul class="menu bg-base-200 min-h-full w-80 p-4">
-          <li><RouterLink to="/" class="btn btn-ghost text-xl">Adarsh T</RouterLink></li>
+        <label for="navbar-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
+        <ul class="menu menu-lg bg-base-200 min-h-full w-80 p-4">
+          <RouterLink to="/" class="btn btn-ghost text-4xl dm-serif-display-font"> A. </RouterLink>
           <li><RouterLink to="/">Home</RouterLink></li>
           <li><RouterLink to="/about">About</RouterLink></li>
           <li><RouterLink to="/contact">Contact</RouterLink></li>
           <li><RouterLink to="/blog">Blog</RouterLink></li>
         </ul>
       </div>
-      <RouterLink to="/" class="btn btn-ghost text-xl">Adarsh T</RouterLink>
+      <RouterLink to="/" class="btn btn-ghost text-4xl dm-serif-display-font"> A. </RouterLink>
     </div>
     <div class="flex-3 hidden lg:flex">
-      <ul class="menu menu-horizontal px-1">
-        <li><RouterLink to="/">Home</RouterLink></li>
-        <li><RouterLink to="/about">About</RouterLink></li>
-        <li><RouterLink to="/contact">Contact</RouterLink></li>
-        <li><RouterLink to="/blog">Blog</RouterLink></li>
+      <ul class="menu menu-lg menu-horizontal px-1">
+        <li><RouterLink to="/" class="text-lg">Home</RouterLink></li>
+        <li><RouterLink to="/about" class="text-lg">About</RouterLink></li>
+        <li><RouterLink to="/contact" class="text-lg">Contact</RouterLink></li>
+        <li><RouterLink to="/blog" class="text-lg">Blog</RouterLink></li>
       </ul>
     </div>
     <div class="flex-none">
-      <label class="swap swap-rotate">
-        <input type="checkbox" class="theme-controller" value="synthwave" v-model="isdark" />
-        <svg
-          class="swap-off h-10 w-10 fill-current"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-        >
-          <path
-            d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"
-          />
-        </svg>
-        <svg
-          class="swap-on h-10 w-10 fill-current"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-        >
-          <path
-            d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z"
-          />
-        </svg>
-      </label>
+      <div class="tooltip tooltip-left" :data-tip="isdark ? 'Light mode' : 'Dark mode'">
+        <label class="toggle text-base-content">
+          <input type="checkbox" class="theme-controller" value="synthwave" v-model="isdark" />
+          <svg aria-label="sun" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <g
+              stroke-linejoin="round"
+              stroke-linecap="round"
+              stroke-width="2"
+              fill="none"
+              stroke="currentColor"
+            >
+              <circle cx="12" cy="12" r="4"></circle>
+              <path d="M12 2v2"></path>
+              <path d="M12 20v2"></path>
+              <path d="m4.93 4.93 1.41 1.41"></path>
+              <path d="m17.66 17.66 1.41 1.41"></path>
+              <path d="M2 12h2"></path>
+              <path d="M20 12h2"></path>
+              <path d="m6.34 17.66-1.41 1.41"></path>
+              <path d="m19.07 4.93-1.41 1.41"></path>
+            </g>
+          </svg>
+          <svg aria-label="moon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <g
+              stroke-linejoin="round"
+              stroke-linecap="round"
+              stroke-width="2"
+              fill="none"
+              stroke="currentColor"
+            >
+              <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path>
+            </g>
+          </svg>
+        </label>
+      </div>
     </div>
   </div>
 </template>
